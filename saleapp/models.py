cd5cd747381cebb223c59cf4e392fa3f9d544b69
unknown <huyphan2792005@gmail.com>
2025-11-14 14:12:00 +0700

    user model

--- saleapp/models.py
+++ saleapp/models.py
@@ -1,40 +1,59 @@
 import json
 
-from sqlalchemy.orm import backref, relationship
-
+from sqlalchemy import Boolean, Enum, String, Integer, Column, DateTime, ForeignKey, Float
+from sqlalchemy.orm import relationship
 from saleapp import db, app
+from datetime import datetime
+from enum import Enum as RoleEnum
+from flask_login import UserMixin
 
+class UserRole(RoleEnum):
+    USER = 1
+    ADMIN = 2
 
 class Base(db.Model):
     __abstract__ = True
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    active = Column(Boolean, default=True)
+    created_date = Column(DateTime,default= datetime.now())
 
     def __str__(self):
         return self.name
 
+class User(Base,UserMixin):
+    username = Column(String(150), nullable=False ,unique=True)
+    password = Column(String(150), nullable = False)
+    avatar = Column(String(300), default="https://icons.iconarchive.com/icons/papirus-team/papirus-status/256/avatar-default-icon.png")
+    user_role = Column(Enum(UserRole), default= UserRole.USER)
+
 class Category(Base):
-    products = db.relationship("Product", backref="category", lazy=True)
+    products = relationship("Product", backref="category", lazy=True)
 
 
 class Product(Base):
-    price = db.Column(db.Float, default=0.0)
-    image = db.Column(db.String(300),default="https://res.cloudinary.com/dy1unykph/image/upload/v1740037805/apple-iphone-16-pro-natural-titanium_lcnlu2.webp")
-    cate_id = db.Column(db.Integer, db.ForeignKey(Category.id), nullable=False)
+    price = Column(Float, default=0.0)
+    image = Column(String(300),default="https://res.cloudinary.com/dy1unykph/image/upload/v1740037805/apple-iphone-16-pro-natural-titanium_lcnlu2.webp")
+    cate_id = Column(Integer, ForeignKey(Category.id), nullable=False)
 
 
-# if __name__=="__main__":
-#     with app.app_context():
-#         #db.create_all()
-#         with open("data/category.json", encoding="utf-8") as f:
-#             cates = json.load(f)
-#             for c in cates:
-#                 db.session.add(Category(**c))
-#             db.session.commit()
-#         with open("data/product.json", encoding="utf-8") as f:
-#             products = json.load(f)
-#             for p in products:
-#                 db.session.add(Product(**p))
-#             db.session.commit()
+if __name__=="__main__":
+    with app.app_context():
+        # db.create_all()
+        # with open("data/category.json", encoding="utf-8") as f:
+        #     cates = json.load(f)
+        #     for c in cates:
+        #         db.session.add(Category(**c))
+        #     db.session.commit()
+        # with open("data/product.json", encoding="utf-8") as f:
+        #     products = json.load(f)
+        #     for p in products:
+        #         db.session.add(Product(**p))
+        #     db.session.commit()
+        with open("data/user.json", encoding="utf-8") as f:
+            users = json.load(f)
+            for u in users:
+                db.session.add(User(**u))
+            db.session.commit()
 
 
